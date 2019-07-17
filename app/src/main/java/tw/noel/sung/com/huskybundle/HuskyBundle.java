a1c050579c4bbf67d86595d323e151065cc28cf2
noel77543 <noel77543@gmail.com>
2019-07-18 00:30:06 +0800

    clean code

--- app/src/main/java/tw/noel/sung/com/huskybundle/HuskyBundle.java
+++ app/src/main/java/tw/noel/sung/com/huskybundle/HuskyBundle.java
@@ -21,69 +21,42 @@ public class HuskyBundle {
             return huskyBundle;
         }
     }
-
     //--------------------
 
     /***
-     *   映射:  接收端 接值
+     *   接收端 接值
      * @param activity
      */
     public void inject(Activity activity) {
-
-
         Bundle bundle = activity.getIntent().getExtras();
         Class aClass = activity.getClass();
+        getValue(activity, aClass, bundle);
 
-
-        if (bundle != null) {
-
-            // 取得所有宣告變數
-            Field[] declaredFields = aClass.getDeclaredFields();
-
-            //逐一取得此Fragment or Activity宣告的所有變數
-            for (Field field : declaredFields) {
-
-                //如果該變數之Annotation為自定義錨點
-                if (field.isAnnotationPresent(GetValue.class)) {
-                    //取得錨點
-                    GetValue getValueAnnotation = field.getAnnotation(GetValue.class);
-
-                    //變數名稱
-                    String filedName = field.getName();
-                    //設置的錨點名稱
-                    String name = getValueAnnotation.name();
-
-
-                    //取得所有bundle的資源
-                    for (String key : bundle.keySet()) {
-                        Object data = bundle.get(key);
-                        //如有指定錨點名稱  || 如沒有指定則直接認物件名稱
-                        if ((!name.equals("") && key.equals(name)) || key.equals(filedName)) {
-                            try {
-                                field.setAccessible(true);
-                                field.set(activity, data);
-                            } catch (IllegalAccessException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                }
-            }
-        }
     }
 
 
     //--------------------
 
     /***
-     *   映射:  接收端 接值
+     *   接收端 接值
      * @param fragment
      */
     public void inject(Fragment fragment) {
-
         Bundle bundle = fragment.getArguments();
         Class aClass = fragment.getClass();
+        getValue(fragment, aClass, bundle);
 
+    }
+
+    //---------------
+
+    /***
+     *  透過映射 取值
+     * @param object
+     * @param aClass
+     * @param bundle
+     */
+    private void getValue(Object object, Class aClass, Bundle bundle) {
         if (bundle != null) {
 
             // 取得所有宣告變數
@@ -110,7 +83,7 @@ public class HuskyBundle {
                         if ((!name.equals("") && key.equals(name)) || key.equals(filedName)) {
                             try {
                                 field.setAccessible(true);
-                                field.set(fragment, data);
+                                field.set(object, data);
                             } catch (IllegalAccessException e) {
                                 e.printStackTrace();
                             }
