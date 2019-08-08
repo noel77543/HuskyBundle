64124524b184afe9b91cce267ca465fdd314a169
noel77543 <noel77543@gmail.com>
2019-08-08 15:49:40 +0800

    clean code

--- app/src/main/java/tw/noel/sung/com/huskybundle/PutValue.java
+++ app/src/main/java/tw/noel/sung/com/huskybundle/PutValue.java
@@ -1,7 +1,6 @@
 package tw.noel.sung.com.huskybundle;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -9,7 +8,6 @@ import java.lang.annotation.Target;
 /**
  * Created by noel on 2019/7/16.
  */
-@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface PutValue {
